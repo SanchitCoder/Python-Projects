ca50f812a63270f4631f7213a1bbcf735f7b4864
SanchitCoder <93979411+SanchitCoder@users.noreply.github.com>
2025-05-10 00:35:44 +0530

    This prgram speak out the weather of the input city on degree celcius

---
+++ Weather.py
@@ -0,0 +1,20 @@
+import requests
+import os
+import json
+import win32com.client as w
+s= w.Dispatch("SAPI.SpVoice")
+
+i=input("Enter the name of city")
+url = f"https://api.weatherapi.com/v1/current.json?key=b73cf79ee3544b30bc3182639250505&q={i}&"
+r=requests.get(url)
+print(r.text)
+dic = json.loads(r.text)
+a= (dic["current"]["temp_c"])
+s.speak(a)
+
+# y=3
+# y=y*2
+# print(y)
+#
+# double  = lambda x: x * 2
+# print(double(3))
